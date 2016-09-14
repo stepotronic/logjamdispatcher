dc80161d812b7d703b5d8d5a70902f07d6b30846
Alexander Mührenberg <alexander.muehrenberg@xing.com>
2016-09-14 15:16:18 +0200

    removed wrong jsonSerialize doc

--- src/Logjam/Message.php
+++ src/Logjam/Message.php
@@ -467,11 +467,7 @@ class Message implements MessageInterface
     }
 
     /**
-     * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
-     * @return mixed data which can be serialized by <b>json_encode</b>,
-     * which is a value of any type other than a resource.
-     * @since 5.4.0
+     * @return array
      */
     public function jsonSerialize()
     {
